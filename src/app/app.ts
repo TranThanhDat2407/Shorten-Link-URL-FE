4ba96b6e286da0fe13e637cac1c78ee88658bb00
TranThanhDat2407 <tranthanhdat2407.email@gmail.com>
2025-12-09 11:18:10 +0700

    add ping actuator/health

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {faTwitter} from '@fortawesome/free-brands-svg-icons/faTwitter';
 import {faGoogle} from '@fortawesome/free-brands-svg-icons/faGoogle';
+import {HttpClient} from '@angular/common/http';
+import {environment} from './environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,14 @@ import {faGoogle} from '@fortawesome/free-brands-svg-icons/faGoogle';
 export class App {
   protected readonly title = signal('short-link-fe');
 
-  constructor(library: FaIconLibrary) {
+  constructor(library: FaIconLibrary,private http: HttpClient) {
     library.addIcons(faTwitter,faGoogle);
   }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.http.get(environment.apiUrl + "/actuator/health").subscribe();
+    }, 5 * 60 * 1000); // 5 phút
+  }
+
 }
