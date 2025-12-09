f14bc4ef7fe28572b3e65710e360ce5a2e4b0947
TranThanhDat2407 <tranthanhdat2407.email@gmail.com>
2025-12-09 15:16:03 +0700

    add check health app components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit, signal} from '@angular/core';
+import {Router, RouterOutlet} from '@angular/router';
 import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {faTwitter} from '@fortawesome/free-brands-svg-icons/faTwitter';
 import {faGoogle} from '@fortawesome/free-brands-svg-icons/faGoogle';
 import {HttpClient} from '@angular/common/http';
 import {environment} from './environments/environment';
+import {HealthCheckService} from './core/services/health-check';
 
 @Component({
   selector: 'app-root',
@@ -12,17 +13,20 @@ import {environment} from './environments/environment';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('short-link-fe');
 
-  constructor(library: FaIconLibrary,private http: HttpClient) {
+  constructor(library: FaIconLibrary,private http: HttpClient,
+              private healthService: HealthCheckService, private router: Router) {
     library.addIcons(faTwitter,faGoogle);
   }
 
   ngOnInit() {
-    setInterval(() => {
-      this.http.get(environment.apiUrl + "/actuator/health").subscribe();
-    }, 5 * 60 * 1000); // 5 phút
+    this.healthService.monitorHealth().subscribe((isUp) => {
+      if (!isUp) {
+        this.router.navigate(['/server-down']);
+      }
+    });
   }
 
 }
